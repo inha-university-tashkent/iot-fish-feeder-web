5016ed26c7d7285cfcae30bb943ea8b46fbce0c1
AMazhidov <majidov3011@gmail.com>
2024-05-11 23:27:08 +0500

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { AngularFireDatabase } from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iot-client';
+  lastTimeFed = '';
+  amount = 0;
+
+  constructor(private db: AngularFireDatabase) {}
+  ngOnInit(): void {
+    this.db.object('last_time_fed').valueChanges()
+    .subscribe(x => {
+      let date = new Date(x as string);
+      this.lastTimeFed = date.toLocaleString();
+    });
+    this.db.object('amount').valueChanges()
+    .subscribe(x => {
+      this.amount = x as number
+    })
+  }
+  feedFish() {
+    this.db.object('should_feed').set(true);
+    this.db.object('last_time_fed').set(new Date().toString());
+  }
+  resetAmount() {
+    this.db.object('amount').set(0);
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    { provide: FIREBASE_OPTIONS, useValue: environment.firebase },
+    provideRouter(routes),
+    provideClientHydration(),
+    importProvidersFrom([
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideFirestore(() => getFirestore()),
+    ]),
+  ],
 };
